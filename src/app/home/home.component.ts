bf60e19d78867da0770b9be01aa40bfc6e303759
phamchi007 <30425936+phamchi007@users.noreply.github.com>
2017-10-30 23:42:41 +0700

    Add home screen

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core"
+
+@Component ({
+    selector:"app-home",
+    templateUrl:'./home.component.html',
+    styleUrls:["../base/css/main.component.css"]
+})
+
+export class HomeComponent{
+    hoten:String = "Chi Pham";
+}
