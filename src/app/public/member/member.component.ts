253193914133f4140780c1cc64df86d4c939a721
vfa-chipm <vfa.chipm@gmail.com>
2017-11-15 15:29:45 +0700

    update code

--- src/app/public/member/member.component.ts
+++ src/app/public/member/member.component.ts
@@ -1,31 +1,29 @@
 import { NgModule, Component , OnInit, Input} from "@angular/core"
 import { Member , MemberData}    from './member';
 import { FormGroup }                 from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 import { DataService } from "../base/data/data.service";
-import { Globals } from "../base/data/data.main";
+
 
 @Component ({
     selector:"app-member",
     templateUrl:'./member.component.html',
     styleUrls:['./member.component.css'],
-    providers: [Globals]
+    
 })
 
 export class PublicMemberComponent implements OnInit{
 
     message: string;
-    constructor(
-        private globals: Globals
-    ) { 
-    }
+    
     ngOnInit() {}
-    form: FormGroup;
     member = new Member(0, '', '');
     memberDatas = MemberDatas;
     
     openCreate(): void{
         this.memberDatas.push(this.member.getMember());
-        this.globals.setdata();
+        
     };
     
     openEdit(item:object): void{
@@ -35,9 +33,6 @@ export class PublicMemberComponent implements OnInit{
     getMember(){
         this.memberDatas;
     }
-    public handleEvent(childData:any){console.log(childData);
-		this.message = childData;
-	}
 }
 
 const MemberDatas: MemberData[] = [];
